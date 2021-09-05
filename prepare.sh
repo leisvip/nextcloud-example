9b1883ce70d98822c5222b0737820c50c5c443d9
csr632 <632882184@qq.com>
2021-09-06 01:11:50 +0800

    improve script

--- prepare.sh
+++ prepare.sh
@@ -1,1 +1,8 @@
-cp ./daemon.json /etc/docker/daemon.json
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn/"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
