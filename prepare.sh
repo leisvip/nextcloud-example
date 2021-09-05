419e2c6556eea8dd3adc3d46c3a3e9668c6c6134
csr632 <632882184@qq.com>
2021-09-06 02:55:08 +0800

    improve prepare script

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# set ip/domain of app
+if [ "$#" -eq  "0" ]
+  then
+    echo "No arguments supplied. App domain name is not set."
+else
+    echo "Hello world"
+    sed -i -e "s/your.domain.name/$1/g" ./docker-compose.yml
+fi
+
+# config docker registry mirrors
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
